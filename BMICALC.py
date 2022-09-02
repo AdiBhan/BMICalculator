d593a29461025773c91b757d59f0fd433f873c5f
Adi Bhan <96316689+AdiBhan@users.noreply.github.com>
2022-09-02 11:21:59 -0400

    Update BMICALC.py

--- BMICALC.py
+++ BMICALC.py
@@ -1,6 +1,6 @@
 from fileinput import close
-from tkinter import *
 from tkinter import messagebox
+from tkinter import *
 
 ''' Author: Adi Bhan'''
 ''' Description: Using tkinter, I created a simple GUI that takes the a user's input and displays their BMI along with the number of pounds they must lose to become healthy'''
@@ -8,7 +8,7 @@ from tkinter import messagebox
 
 # Placeholders
 
-Version = '1.00'
+Version = '1.0.1'
 font = 'Arial'
 background_color = 'green'
 header_fontsize = 15
@@ -17,7 +17,6 @@ text_fontsize = 9
 GUI = Tk()
 
 ## Setting the size , color, title, logo and border of the GUI
-
 
 # Title
 GUI.title('BMI Calculator')  
@@ -33,9 +32,7 @@ GUI.resizable(False, False)
 # Logo
 GUI.iconbitmap('c:/Testing/BMICALC_ICON.ICO')
 
-
-
-## A welcome header that shares the current version with the user
+## Welcome Header
 
 GUI_WelcomeHeader = Label(GUI,
                           text = ('BMICalculator ' + Version),
@@ -46,9 +43,7 @@ GUI_WelcomeHeader = Label(GUI,
                           relief="solid",
                           ).place(x = 150, y = 5)
 
-
-
-# Using scales to get user input for weight and height
+# Height and Weight scales 
 
 Height_Scale = Scale(
                     from_= 0, 
@@ -59,7 +54,6 @@ Height_Scale = Scale(
                     relief="solid",
                     length = 250, 
                     orient= HORIZONTAL)
-
 
 Height_Scale.place(x=100, y=60)
 
@@ -75,7 +69,7 @@ Weight_Scale = Scale(
 
 Weight_Scale.place(x=100, y=130)
 
-# Labels for weight and height scale
+# Labels to help user identify the height and weight scale
 
 Height_Label = Label(
     text = "Height (Inches): ", 
@@ -84,7 +78,6 @@ Height_Label = Label(
     font = (font, text_fontsize),
     ).place(x = 190, y = 35)
 
-
 Weight_Label = Label(
     text = "Weight (lbs): ", 
     bg = background_color,
@@ -92,63 +85,67 @@ Weight_Label = Label(
     font = (font, text_fontsize),
     ).place(x = 190, y = 108)
 
-## Function calculates BMI using the CDC BMI Formula
+## Function calculates BMI using the CDC BMI Formula (https://www.cdc.gov/healthyweight/assessing/bmi/adult_bmi/index.html)
 
 def BMICalc():
     
+# If user inputs 0 as value for weight or height, program will throw an error message at them
+    
+    if Height_Scale.get() == 0 or Weight_Scale.get() == 0:
+        messagebox.showwarning("Error 101", "Your height and/or weight value is currently set to 0. Please change it to an appropriate number!")
+    
+    else:    
+    
     # Basic CDC BMI Formula 
     
-    Formula = (((float(Weight_Scale.get()) / (float(Height_Scale.get())) ** 2) ) * 703 )
+        Formula = (((float(Weight_Scale.get()) / (float(Height_Scale.get())) ** 2) ) * 703 )
     
-    Label(text = "Your BMI is " + str(round(float(Formula),2)) + "!").place(x=180, y=215,)
+        Label(text = "Your BMI is " + str(round(float(Formula),2)) + "!").place(x=180, y=215,)
     
-## The formula below calculating the amount of pounds a user needs to obtain an healthy BMI (Reach 25 BMI). 
+    ## The formula below calculating the amount of pounds a user needs to obtain an healthy BMI (Reach 25 BMI). 
     ## I solved this by reverse engineering the CDC BMI Formula. This formula assumes that the user's height stays the same (target audience is adults).
     
-    BMI_To_Weight_Formula = str(abs(round(float(((((18.5 - Formula ) / 703) * (float(Height_Scale.get())) ** 2))),2)))
+        BMI_To_Weight_Formula = str(abs(round(float(((((18.5 - Formula ) / 703) * (float(Height_Scale.get())) ** 2))),2)))
     
-## Based on the user's BMI, the program will display special messages telling them what BMI Weight Status they are in and how many pounds they must lose to reach a lower weight status.
+    ## Based on the user's BMI, the program will display special messages telling them what BMI Weight Status they are in and how many pounds they must lose to reach a lower weight status.
     
-    Weight_Gain_Message = "You must gain " + BMI_To_Weight_Formula + " pounds to reach a healthy BMI!"
-    Weight_Loss_Message =  "You must lose " + BMI_To_Weight_Formula + " pounds to reach a healthy BMI!"
-    
-    
-    ## Gives an error message if Height or Weight is 0
-    
+        Weight_Gain_Message = "You must gain " + BMI_To_Weight_Formula + " pounds to reach a healthy BMI!"
+        Weight_Loss_Message =  "You must lose " + BMI_To_Weight_Formula + " pounds to reach a healthy BMI!"
+       
+    ## User is sent different messages based on their BMI
 
-    
-    if Formula < 18.5:
+        if Formula < 18.5:
         
-        Label(text = "WARNING: Underweight! Your BMI is below 18.5 and therefore you are underweight.", foreground = 'Blue').place(x=10, y=245,)
+            Label(text = "WARNING: Underweight! Your BMI is below 18.5 and therefore you are underweight.", foreground = 'Blue').place(x=10, y=245,)
                     
-        Label(text = Weight_Gain_Message, foreground = 'Blue').place(x=10, y=265,)
+            Label(text = Weight_Gain_Message, foreground = 'Blue').place(x=10, y=265,)
                         
-    elif 18.5 <= Formula <= 24.9:
+        elif 18.5 <= Formula <= 24.9:
         
-        Label(text = "CONGRATS: Healthy! You are at a healthy BMI.", foreground = 'Green').place(x=10, y=245,)
+            Label(text = "CONGRATS: Healthy! You are at a healthy BMI.", foreground = 'Green').place(x=10, y=245,)
             
-    elif 25 <= Formula <= 29.9:
+        elif 25 <= Formula <= 29.9:
         
-        Label(text = "WARNING: Overweight! You are overweight.", foreground = 'Red').place(x=10, y=245,)
+            Label(text = "WARNING: Overweight! You are overweight.", foreground = 'Red').place(x=10, y=245,)
         
-        Label(text = Weight_Loss_Message, foreground = 'Red').place(x=10, y=265,)
+            Label(text = Weight_Loss_Message, foreground = 'Red').place(x=10, y=265,)
 
-    elif Formula >= 30:
+        elif Formula >= 30:
         
-        Label(text = "SEVERE WARNING: OBESE! You BMI is above 30 and therefore you are obese.", foreground = 'Dark Red').place(x=10, y=245,)
+            Label(text = "SEVERE WARNING: OBESE! You BMI is above 30 and therefore you are obese.", foreground = 'Dark Red').place(x=10, y=245,)
                
-        Label(text = Weight_Loss_Message, foreground = 'Dark Red').place(x=10, y=265,)
-        
-        
-# An submit button which when a user clicks runs the BMICalc Function (Scroll Up)
-
+            Label(text = Weight_Loss_Message, foreground = 'Dark Red').place(x=10, y=265,)
+   
+# Submit button runs the BMICalc Function when clicked    
+    
 Submit = Button(GUI, 
                 text = 'Calculate BMI!', 
                 width = 13, 
                 command = BMICalc 
                 ).place(x=180, y=180,)
 
-## A close button that allows the user to quit the GUI
+## Close button allows user to exit the GUI
+
 CLOSE_Button = Button(
                       GUI, 
                       text = 'CLOSE',
@@ -158,8 +155,3 @@ CLOSE_Button = Button(
                       ).pack(side= BOTTOM)
 
 GUI.mainloop()
-
-
-
-    
-
